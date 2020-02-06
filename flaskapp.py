9665a3c8c1389cd5e068c3fefa0f968bf5aea43f
Abhishek Tiwari <39875465+becomeahacker@users.noreply.github.com>
2020-02-06 21:16:10 +0530

    Add files via upload

--- flaskapp.py
+++ flaskapp.py
@@ -2,11 +2,11 @@ from flask import Flask, request, render_template
 import requests
 from bs4 import BeautifulSoup
 
-
 app = Flask(__name__)
 #Vars
 Theme = 'dracula'
 problemQuestion =''
+topics = []
 Languages = {
 	'C' : 'C' ,
 	'Cpp' : 'C++',
@@ -31,6 +31,64 @@ LangHLModes = {
 	'Python3' : 'text/x-python' , 
 	'Scala' : 'text/x-scala'
 }
+
+
+
+@app.route('/')
+def my_form():
+	global problemQuestion
+	DisplayOutput = 'none'
+	if problemQuestion == '' :
+		problemQuestion = Scraper("rotate-array-by-n-elements/0")
+	return render_template('index.html' , Languages = Languages , SelectedLanguage ='C' , DisplayOutput = DisplayOutput , LangHLModes = LangHLModes ,Theme = Theme , problemQuestion = problemQuestion)
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+	global problemQuestion
+	SourceCode = request.form['code']
+	CustomInput = request.form['input']
+	Lang = request.form['lang']
+	print("Form : " , request.form)
+	Save = 'true'
+	Output = ''
+	DisplayOutput='none'
+	
+	if request.form['Query']=='submit' :
+		Output = Compile(SourceCode , Lang , CustomInput , Save)
+		DisplayOutput = 'block'
+	elif request.form['Query']=='hide_output' :
+		DisplayOutput = 'none'
+	elif request.form['Query']=='show_output' :
+		DisplayOutput = 'block'
+	if problemQuestion == '' :
+		problemQuestion = Scraper("rotate-array-by-n-elements/0")
+	#print(problemQuestion)
+	return render_template('index.html' , SourceCode=SourceCode , CustomInput=CustomInput, Languages = Languages , SelectedLanguage= Lang , Output = Output , DisplayOutput = DisplayOutput , LangHLModes = LangHLModes , Theme = Theme, problemQuestion=problemQuestion)
+
+if __name__ == "__main__" : 
+	app.run(debug = True)
+
+'''
+@app.route('/home') 
+def show_topics() :
+	global topics
+	if len(topics) == 0 : 
+		topics = ScrapeTopics()
+	return render_template('topics.html' , topics = topics)
+
+@app.route('/home' , methods=['POST'])
+def show_topics_form() :
+	global topics
+	link = request.form['Query']
+	print(topics[link][0] , topics[link][1])
+	return render_template('home.html')
+'''
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+#------------------------FUNCTIONS ---------------------------------------------------------
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+
 def Compile(Code , Lang , Input, Save) :
 	url = 'https://ide.geeksforgeeks.org/main.php'
 	data = {
@@ -41,7 +99,7 @@ def Compile(Code , Lang , Input, Save) :
 	}
 	response = requests.post(url,data=data)
 	Output = response.json()
-	res = ["\nSubmission Results :"]
+	res = []
 	if Output['compResult']=='S' and Output['valid']=='1' :
 		if Output['cmpError']!='' :
 			res.append('Compilation Failed!')
@@ -100,7 +158,7 @@ def Scraper( ProblemCode ) :
 			tabContent[i] = tabContent[i].text
 			pDesc.append(tabContent[i].strip().split('\n'))
 		problem['pDesc'] = pDesc
-		print(pDesc)
+		#print(pDesc)
 		problem['pValid'] = '1'
 		return problem	
 	except ConnectionError:
@@ -108,31 +166,23 @@ def Scraper( ProblemCode ) :
 	problem = {'pValid' : '0'}
 	return problem
 
+def ScrapeTopics() :
+	try :
+		response = requests.get('https://practice.geeksforgeeks.org/')
+		soup = BeautifulSoup(response.content , 'html5lib')
+		Category = soup.find_all('div' , attrs={'class' : 'col-xs-12 col-sm-6 col-md-3 itemInnerDiv'})
+		topics = []
+		for i in Category :
+			if(i.text.strip()=="") :
+				pass
+			else :
+				topics.append([ str(i.text.strip()) ,  str(i.a['href'].strip()) ])
+		return topics	
+	except :
+		print("Please Check your network connection!!")
+		return []
 
-@app.route('/')
-def my_form():
-	global problemQuestion
-	DisplayOutput = 'none'
-	if problemQuestion == '' :
-		problemQuestion = Scraper("kth-smallest-element/0")
-	return render_template('index.html' , Languages = Languages , SelectedLanguage ='C' , DisplayOutput = DisplayOutput , LangHLModes = LangHLModes ,Theme = Theme , problemQuestion = problemQuestion)
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-	global problemQuestion
-	SourceCode = request.form['code']
-	CustomInput = request.form['input']
-	Lang = request.form['lang']
-	print("Form : " , request.form)
-	Save = 'true'
-	Output = ''
-	DisplayOutput='none'
-	if request.form['LangChanged']=='false' :
-		Output = Compile(SourceCode , Lang , CustomInput , Save)
-		DisplayOutput = 'block'
-	if problemQuestion == '' :
-		problemQuestion = Scraper("kth-smallest-element/0")
-	return render_template('index.html' , SourceCode=SourceCode , CustomInput=CustomInput, Languages = Languages , SelectedLanguage= Lang , Output = Output , DisplayOutput = DisplayOutput , LangHLModes = LangHLModes , Theme = Theme , problemQuestion = problemQuestion)
 
-if __name__ == "__main__" : 
-	app.run(debug = True)
+#repair scraper function to return    name : link dictionary 
+#function returns name and corresponding names link is then scraped 
